a871036b32ec770086c55f2dd8a640e188ffefef
patkamon <patkamon.a@ku.th>
2023-08-06 17:47:43 +0700

    Clean use effect

--- src/components/Balances.tsx
+++ src/components/Balances.tsx
@@ -3,45 +3,32 @@ import { useState, useEffect } from "react";
 
 
 
-export function Balances(account :Object) {
+export function Balances(account :{account: string}) {
 
   const [oplist, setOpList] = useState([])
   const [polylist, setPolyList] = useState([])
 
-  useEffect(()=>{
-    console.log("Hello", account.account)
+  const fetchData = async (chain : string, address: string, setstate: React.Dispatch<React.SetStateAction<never[]>> ) => {
     let headers = new Headers();
-    headers.set('Authorization', "Bearer ")
-
-    const fetchData = async () => {
-      const data = await fetch(`https://api.covalenthq.com/v1/optimism-goerli/address/${account.account}/balances_v2/?`, {method: 'GET', headers: headers})
-      .then((resp) => resp.json())
-      .then((data) => {
-        console.log(data)
-        setOpList(data.data.items)
-        });
-      }
-
-    const fetchData2 = async () => {
-          const data = await fetch(`https://api.covalenthq.com/v1/matic-mumbai/address/${account.account}/balances_v2/?`, {method: 'GET', headers: headers})
-          .then((resp) => resp.json())
-          .then((data) => {
-            console.log(data)
-            setPolyList(data.data.items)
-            });
-
-
+    headers.set('Authorization', "Bearer cqt_rQFY9PtDfbqGrmqD7qkbqC9R3ccJ")
+    const data = await fetch(`https://api.covalenthq.com/v1/${chain}/address/${address}/balances_v2/?`, {method: 'GET', headers: headers})
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data)
+      setstate(data.data.items)
+      });
     }
 
+  useEffect(()=>{
+    fetchData("optimism-goerli",account.account, setOpList)
+    // make sure to catch any error
+    .catch(console.error);
+
     // call the function
-    fetchData()
+    fetchData("matic-mumbai",account.account,  setPolyList)
       // make sure to catch any error
       .catch(console.error);
 
-  // call the function
-    fetchData2()
-    // make sure to catch any error
-    .catch(console.error);
 
   },[])
 
@@ -50,16 +37,16 @@ export function Balances(account :Object) {
       <h2>Balances</h2>
 
       <h2>OPTIMISM GOERLI</h2>
-      {oplist.map((l)=>{
-          return <div key={l.contract_name}>
-              {l.contract_name}<br/>
-              {l.balance}
-              </div>
+      {oplist.map((l,index)=>{
+           return  l.contract_name != null ? <div key={l.contract_name + index}>
+           {l.contract_name}<br/>
+           {l.balance}
+           </div> : <></>
       })}
 
       <h2>POLYGON MUMBAI</h2>
-      {polylist.map((l)=>{
-            return  l.contract_name != null ? <div key={l.contract_name}>
+      {polylist.map((l,index)=>{
+            return  l.contract_name != null ? <div key={l.contract_name + index}>
               {l.contract_name}<br/>
               {l.balance}
               </div> : <></>
